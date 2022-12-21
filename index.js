48b8e5b96561c74c8408767e36252b3b453a7f10
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-12-21 23:17:19 +1100

    Notion API setup and testing

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-require("dotenv").config();
+import * as dotenv from "dotenv";
+dotenv.config();
+import { Client } from "@notionhq/client";
+import express from "express";
 
-const express = require("express");
 const app = express();
 const port = 3000;
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+    return response.url;
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
+async function getPage(pageId) {
+  const response = await notion.pages.retrieve({ page_id: pageId });
+  console.log(response);
+}
+
+async function getBlock(blockId) {
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  console.log(response);
+}
+
+let url = await addItem("Yurts in Big Sur, California");
 
 app.get("/", (req, res) => {
-  res.send(process.env.NOTION_KEY);
+  res.send("Hello world!");
 });
 
 app.get("/gym/push", (req, res) => {
-  res.send("Push day!");
+  res.redirect(url);
 });
 
 app.get("/gym/pull", (req, res) => {
