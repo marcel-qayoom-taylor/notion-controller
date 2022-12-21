257b0786b121c779ae2a37860289c16d31e34dbe
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-12-21 21:30:00 +1100

    Setup dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(process.env.NOTION_KEY);
 });
 
 app.get("/gym/push", (req, res) => {
