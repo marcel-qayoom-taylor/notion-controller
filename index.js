6597b3cc721313669c0205957206ce26b8e062c7
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-12-22 17:47:10 +1100

    Favicon broken

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import createGymPage from "./src/gym/gym-controller.js";
+import path from "path";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
+app.use(ignoreFavicon);
+
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl.includes("favicon.ico")) {
+    res.status(204).end();
+  }
+  next();
+}
 
 // app.use("/gym", createGymPage);
 app.get("/gym/:muscleGroup", async (req, res) => {
@@ -22,7 +32,7 @@ app.get("/gym/:muscleGroup", async (req, res) => {
 app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.get("/", (req, res) => {
-  res.sendFile("/index.html");
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
